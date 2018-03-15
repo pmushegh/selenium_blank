23a8c21c2ef34f2a6d7844b3f1797b7f872601a7
Mushegh Petrosyan <p.mushegh@gmail.com>
2018-03-15 18:57:49 +0400

    Added log4j2 to project.

---
+++ src/main/java/utils/Log.java
@@ -0,0 +1,37 @@
+package utils;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+public class Log {
+
+    private static final Logger logger = LogManager.getLogger(Log.class);
+
+    public static void info(String strMessage) {
+        logger.info(strMessage);
+    }
+
+    public static void warn(String strMessage) {
+        logger.warn(strMessage);
+    }
+
+    public static void error(String strMessage) {
+        logger.error(strMessage);
+    }
+
+    public static void  fatal(String strMessage) {
+        logger.fatal(strMessage);
+    }
+
+    public static void debug(String strMessage) {
+        logger.debug(strMessage);
+    }
+
+    public static void testCaseStart(String strTestCaseName) {
+        logger.info("### Test case " + strTestCaseName + " start.");
+    }
+
+    public static void testCaseEnd(String strTestCaseName) {
+        logger.info("### Test case" + strTestCaseName + " end.");
+    }
+}
--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,9 +1,11 @@
 import org.testng.annotations.Test;
+import utils.Log;
 
 public class SampleTest extends BaseTest {
 
     @Test
     public void someTest() {
+        Log.testCaseStart("Sample test");
         samplePage.open();
     }
 }
