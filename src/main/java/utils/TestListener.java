1fa6b6a3725a31efe5b44c3892671b8b8b8a4cd4
Mushegh Petrosyan <p.mushegh@gmail.com>
2019-06-04 18:23:14 +0400

    Add code to be chaged for screenshot taking

--- src/main/java/utils/TestListener.java
+++ src/main/java/utils/TestListener.java
@@ -1,5 +1,9 @@
 package utils;
 
+import io.qameta.allure.Attachment;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
@@ -44,4 +48,19 @@ public class TestListener implements ITestListener {
     private static String getTestMethodName(ITestResult result) {
         return result.getMethod().getConstructorOrMethod().getName();
     }
+
+//    @Attachment(value = "Page screenshot", type = "image/png")
+//    public byte[] saveScreenshotPNG(WebDriver webDriver) {
+//        return ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.BYTES);
+//    }
+//
+//    public void takeScreenshot(ITestResult iTestResult, String strTestPhase) {
+//        Object testClass = iTestResult.getInstance();
+//        Object webDriver = ((BaseTest) testClass).driver.get();
+//        if (webDriver instanceof WebDriver) {
+//            saveScreenshotPNG((WebDriver) webDriver);
+//        } else {
+//            Log.error("TL: ## " + getTestMethodName(iTestResult) + " " + strTestPhase + ", something went wrong during get of the WebDriver from Test Result");
+//        }
+//    }
 }
